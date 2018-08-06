4481dd0558cf071950d5c6b964c12fcb45cdba9d
uskcse <33146839+uskcse@users.noreply.github.com>
2018-08-06 15:50:43 +0530

    Add file via upload

---
+++ jdoodle.c
@@ -0,0 +1,26 @@
+﻿#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+void main()
+{
+       char c;
+       printf("Input:");
+       scanf("%c",&c);
+       printf("Output:");
+       if(isalpha(c))
+       {
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+            {
+                 printf("Vowel");
+            }
+            else
+            {
+                 printf("Consonant");
+            }
+       }
+       else
+       {
+            printf("Invalid");
+        }
+
+}
