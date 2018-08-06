0eb1be3cb50890a44e263f3eb2a755df00d94683
uskcse <33146839+uskcse@users.noreply.github.com>
2018-08-06 16:09:42 +0530

    Add file via upload

---
+++ jdoodle (1).c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+void main() {
+   char c;
+   printf("input:");
+   scanf("%c",&c);
+   printf("\noutput:");
+   if(isalpha(c))
+   {
+       printf("\nalphabet");
+   }
+   else
+   {
+       printf("\nNo");
+}
+}
